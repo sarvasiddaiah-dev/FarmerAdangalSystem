a9791228eefb567da4b27f38ee6ef67e7024eb95
Sarvasiddaiah <sarvasiddaiah@gmail.com>
2026-08-10 15:36:26 +0530

    Pattern and Dependency Injection

---
+++ AuthenticationService.API/Controllers/ConfigurationTestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AuthenticationService.API.Configuration;
+using Microsoft.Extensions.Options;
+namespace AuthenticationService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigurationTestController : ControllerBase
+    {
+        private readonly JwtSettings _jwtSettings;
+        private readonly SmsSettings _smsSettings;
+        public ConfigurationTestController(IOptions<JwtSettings> jwtOptions,IOptions<SmsSettings> smsOptions)
+        {
+            _jwtSettings = jwtOptions.Value;
+            _smsSettings = smsOptions.Value;
+        }
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok(new
+            {
+                JwtConfigured = !string.IsNullOrWhiteSpace(_jwtSettings.SecretKey),
+                JwtIssuer=_jwtSettings.SecretKey,
+                smsProvider=_smsSettings.Provider,
+                smsSenderId=_smsSettings.SenderId,
+             });
+                
+
+        }
+    }
+}
--- AuthenticationService.API/Program.cs
+++ AuthenticationService.API/Program.cs
@@ -1,6 +1,14 @@
+using AuthenticationService.API.Configuration;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection(ApplicationSettings.SectionName));
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(DatabaseSettings.SectionName));
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection(JwtSettings.SectionName));
+builder.Services.Configure<SmsSettings>(builder.Configuration.GetSection(SmsSettings.SectionName));
+builder.Services.Configure<SwaggerSettings>(builder.Configuration.GetSection(SwaggerSettings.SectionName));
+builder.Services.Configure<SerilogSettings>(builder.Configuration.GetSection(SerilogSettings.SectionName));
 
 builder.Services.AddControllers();
 
