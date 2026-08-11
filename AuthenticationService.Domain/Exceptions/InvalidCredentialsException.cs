e017d703054653b82534226ab16e030c5ff2c27a
Sarvasiddaiah <sarvasiddaiah@gmail.com>
2026-08-11 11:41:31 +0530

    Added domain exception handling

---
+++ AuthenticationService.Domain/Exceptions/InvalidCredentialsException.cs
@@ -0,0 +1,16 @@
+﻿using AuthenticationService.Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationService.Domain.Exceptions
+{
+    public class InvalidCredentialsException: DomainException
+    {
+        public InvalidCredentialsException():base(ErrorMessages.InvalidCredentials)
+        { 
+        }
+    }
+}
---
+++ AuthenticationService.Domain/Exceptions/InvalidOtpException.cs
@@ -0,0 +1,17 @@
+﻿using AuthenticationService.Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationService.Domain.Exceptions
+{
+    public  class InvalidOtpException:DomainException
+    {
+        public InvalidOtpException():base(ErrorMessages.InvalidOtp)
+        {
+            
+        }
+    }
+}
